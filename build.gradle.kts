66e52a9132ca6cc93f83da0082bea0b6ba904243
markrybaltovskij <markrybaltovskij@gmail.com>
2026-05-13 17:50:07 +0300

    fix: убрал лишнюю секьюрити конфигурацию внутри сервиса

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-security")
 
     // For Kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -44,7 +43,6 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.kafka:spring-kafka-test")
-    testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation(kotlin("stdlib"))
 }
