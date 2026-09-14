0225c21954b0098fb8ca9d76679b9771112db270
artemka2003 <86artem2003@mail.ru>
2026-09-14 19:32:10 +0300

    fix: добавил обозначение как task_type для выходного значения TaskType

--- src/main/kotlin/com/itmentorcommunityplatform/telegrambotadapter/dto/response/TaskDto.kt
+++ src/main/kotlin/com/itmentorcommunityplatform/telegrambotadapter/dto/response/TaskDto.kt
@@ -1,10 +1,11 @@
 package com.itmentorcommunityplatform.telegrambotadapter.dto.response
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonRawValue
-import com.fasterxml.jackson.databind.JsonNode
 
 
 data class TaskDto(
+    @JsonProperty("task_type")
     val taskType: String,
     @JsonRawValue
     val payload: String,
